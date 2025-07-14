4219ef1a9ae9feb00fa6047e3962b69f2c6f37fc
clip <mjustineguiaz@gmail.com>
2025-07-14 11:51:45 +0800

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Routes, Route} from 'react'
 import './index.css'
 import App from './App'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<App/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )

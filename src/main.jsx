06cdd85e3cff844e4511cf89a7445225f7676134
clip <mjustineguiaz@gmail.com>
2025-07-14 12:21:38 +0800

    a

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='process.env.PUBLIC_URL'>
       <App />
     </BrowserRouter>
   </StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/',
   plugins: [
     react(),
     tailwindcss()],
+  base: '/my-portfolio',  
 })

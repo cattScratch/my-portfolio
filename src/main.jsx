b6e4355550b7cc1e7c503e88502517e14e6e233e
clip <mjustineguiaz@gmail.com>
2025-07-14 12:26:47 +0800

    s

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='process.env.PUBLIC_URL'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>

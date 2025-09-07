cada8a915f8dd8cbdfb6a36e401c17a84039ec01
clip <mjustineguiaz@gmail.com>
2025-09-07 15:47:23 +0800

    .

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,6 @@ import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path='*' element={<App />}/>
-      </Routes>
-    </HashRouter>
+   <App />
   </StrictMode>
 );
